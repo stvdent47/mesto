e949f548c6b46472038098a3ac5bb8ef0de8aed7
Anton Rodionov <stvdent47@gmail.com>
2020-07-05 20:36:40 +0300

    finished with the fixes

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,29 +1,31 @@
-window.addEventListener('load', () => {
-  let editButton = document.querySelector('.edit-button');
-  let popUp = document.querySelector('.popup');
-  let closeButton = document.querySelector('.popup__close-button');
-  let saveButton = document.querySelector('.popup__submit');
-  let nameField = document.querySelector('.profile__name');
-  let jobField = document.querySelector('.profile__job');
-  let nameInput = document.querySelector('#nameInput');
-  let jobInput = document.querySelector('#jobInput');
+let editButton = document.querySelector('.profile__edit-button');
+let popUp = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close-button');
+let saveButton = document.querySelector('.popup__submit');
+let nameField = document.querySelector('.profile__name');
+let jobField = document.querySelector('.profile__job');
+let nameInput = document.querySelector('#name-input');
+let jobInput = document.querySelector('#job-input');
+let popupForm = document.querySelector('.popup__form');
 
-  nameInput.value = nameField.textContent;
-  jobInput.value = jobField.textContent;
+const togglePopup = () => {
+  if (!popUp.classList.contains('popup opened')) {
+    nameInput.value = nameField.textContent;
+    jobInput.value = jobField.textContent;
+  }
+  popUp.classList.toggle('popup_opened');
+}
 
-  editButton.addEventListener('click', () => {
-    popUp.classList.toggle('popup_opened');
-  })
+editButton.addEventListener('click', togglePopup);
 
-  closeButton.addEventListener('click', () => {
-    popUp.classList.toggle('popup_opened');
-  })
+closeButton.addEventListener('click', togglePopup);
 
-  saveButton.addEventListener('click', (e) => {
-    e.preventDefault();
+const submitHandler = (e) => {
+  e.preventDefault();
 
-    nameField.textContent = nameInput.value;
-    jobField.textContent = jobInput.value;
-    popUp.classList.toggle('popup_opened');
-  })
-})
+  nameField.textContent = nameInput.value;
+  jobField.textContent = jobInput.value;
+  togglePopup();
+}
+
+popupForm.addEventListener('submit', submitHandler);
