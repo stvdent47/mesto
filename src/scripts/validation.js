d3f1683dccec1496c2647e071dcc2ff942776d1d
Anton Rodionov <stvdent47@gmail.com>
2020-08-05 14:34:14 +0300

    changed class names for input error message

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -61,5 +61,5 @@ enableValidation({
   submitButtonSelector: '.popup__button',
   inactiveButtonClass: 'popup__button_disabled',
   inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
+  errorClass: 'popup__input-error-message_visible'
 });
